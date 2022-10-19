8cffe802fe2b2e82fb88453f8e97e8635f150013
Stay-Classy <92498354+Stay-Classy@users.noreply.github.com>
2022-10-19 16:25:50 +1000

    Required files/folders
    Adding required files and folders to operate image retrieval from QLD traffic to s3.

---
+++ libs/s3Client.js
@@ -0,0 +1,8 @@
+require('dotenv').config();
+// Create service client module using ES6 syntax.
+const { S3Client } = require ("@aws-sdk/client-s3");
+// Set the AWS Region.
+const REGION = process.env.REGION;
+// Create an Amazon S3 service client object.
+const s3Client = new S3Client({ region: REGION });
+module.exports = { s3Client };
